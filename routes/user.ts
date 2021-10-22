14da55f8efeb72d2cf65e8f01a3ac3de468c417d
LuisPedroChaves <luispedrochaves@hotmail.com>
2021-10-22 17:10:24 -0600

    CORRECCIONES Y CAMBIOS SOLICITADOS

--- routes/user.ts
+++ routes/user.ts
@@ -6,10 +6,10 @@ import User from '../models/user';
 // import mongoose from 'mongoose';
 // var ObjectId = mongoose.Types.ObjectId;
 
-const userRouter = Router();
+const USER_ROUTER = Router();
 
-/* #region  GET */
-userRouter.get('/', mdAuth, (req: Request, res: Response) => {
+/* #region  GET'S */
+USER_ROUTER.get('/', mdAuth, (req: Request, res: Response) => {
 	User.find(
 		{
 			deleted: false,
@@ -34,10 +34,9 @@ userRouter.get('/', mdAuth, (req: Request, res: Response) => {
 			});
 		});
 });
-/* #endregion */
 
-/* #region  GET / ID */
-userRouter.get('/user/:id', mdAuth, (req: Request, res: Response) => {
+// by ID
+USER_ROUTER.get('/user/:id', mdAuth, (req: Request, res: Response) => {
 	const id = req.params.id;
 
     User.findById(id, (err, user) => {
@@ -68,8 +67,8 @@ userRouter.get('/user/:id', mdAuth, (req: Request, res: Response) => {
 });
 /* #endregion */
 
-/* #region  PUT */
-userRouter.put('/:id', mdAuth, (req: Request, res: Response) => {
+/* #region  PUT'S */
+USER_ROUTER.put('/:id', mdAuth, (req: Request, res: Response) => {
 	const id = req.params.id;
 	const body = req.body;
 
@@ -116,10 +115,62 @@ userRouter.put('/:id', mdAuth, (req: Request, res: Response) => {
 		});
 	});
 });
+
+USER_ROUTER.put('/changepass/:id', mdAuth, (req: Request, res: Response) => {
+	const id = req.params.id;
+	const body = req.body;
+
+	User.findById(id, (err, user) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				mensaje: 'Error al buscar usuario',
+				errors: err,
+			});
+		}
+
+		if (!user) {
+			return res.status(400).json({
+				ok: false,
+				mensaje: 'El usuario con el id' + id + ' no existe',
+				errors: {
+					message: 'No existe un usuario con ese ID',
+				},
+			});
+		}
+
+		if (!bcrypt.compareSync(body.oldPassword, user.password)) {
+			return res.status(400).json({
+				ok: false,
+				mensaje: 'Contraseña Incorrecta',
+				errors: err,
+			});
+		}
+
+		user.password = bcrypt.hashSync(body.newPassword, 10);
+
+		user.save((err, userS) => {
+			if (err) {
+				return res.status(400).json({
+					ok: false,
+					mensaje: 'Error al actualizar usuario',
+					errors: err,
+				});
+			}
+
+			userS.password = '********';
+
+			res.status(200).json({
+				ok: true,
+				user: userS,
+			});
+		});
+	});
+});
 /* #endregion */
 
 /* #region  DELETE */
-userRouter.delete('/:id', mdAuth, (req: Request, res: Response) => {
+USER_ROUTER.delete('/:id', mdAuth, (req: Request, res: Response) => {
 	const id = req.params.id;
 
 	User.findById(id, (err, user) => {
@@ -163,7 +214,7 @@ userRouter.delete('/:id', mdAuth, (req: Request, res: Response) => {
 /* #endregion */
 
 /* #region  LOGIN */
-userRouter.post('/login', mdAuth,  (req: Request, res: Response) => {
+USER_ROUTER.post('/login', mdAuth,  (req: Request, res: Response) => {
 	const body = req.body;
 
 	User.findOne({
@@ -213,7 +264,7 @@ userRouter.post('/login', mdAuth,  (req: Request, res: Response) => {
 /* #endregion */
 
 /* #region  POST cellar */
-userRouter.post('/', mdAuth, (req: Request, res: Response) => {
+USER_ROUTER.post('/', mdAuth, (req: Request, res: Response) => {
 	const body = req.body;
 
 	const user = new User({
@@ -244,4 +295,4 @@ userRouter.post('/', mdAuth, (req: Request, res: Response) => {
 });
 /* #endregion */
 
-export default userRouter;
+export default USER_ROUTER;
