672f13c95c8d8eceaa23726be540d99cb4fd9fd4
LuisPedroChaves <luispedrochaves@hotmail.com>
2021-12-06 16:09:07 -0600

    Anulando carga de archivos en VENTAS

--- routes/tempSale.ts
+++ routes/tempSale.ts
@@ -259,6 +259,105 @@ TEMP_SALE_ROUTER.post('/xlsx', (req: Request, res: Response) => {
         });
     });
 });
+
+TEMP_SALE_ROUTER.post('/xlsx/delete', (req: Request, res: Response) => {
+    const BODY = req.body;
+
+    // Sino envia ningún archivo
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'No Selecciono nada',
+            errors: { message: 'Debe de seleccionar un archivo' }
+        });
+    }
+
+    // Obtener nombre y la extensión del archivo
+    const FILE: any = req.files.archivo;
+    const NAME_FILE = FILE.name.split('.');
+    const EXT_FILE = NAME_FILE[NAME_FILE.length - 1];
+
+    // Extensiones permitidas
+    const VALID_EXTS = ['xlsx'];
+
+    if (VALID_EXTS.indexOf(EXT_FILE) < 0) {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'Extensión no válida',
+            errors: { message: 'Las extensiones válidas son: ' + VALID_EXTS.join(', ') }
+        });
+    }
+
+    // Nombre del archivo personalizado
+    const NEW_NAME_FILE = `${new Date().getMilliseconds()}.${EXT_FILE}`;
+
+    // Mover el archivo de la memoria temporal a un path
+    const PATH = `./uploads/temp/${NEW_NAME_FILE}`;
+
+    FILE.mv(PATH, async (err: any) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al mover archivo',
+                errors: err
+            });
+        }
+
+        const DOC = xlsx.parse(PATH);
+
+        let code = 1;
+        let errors: any[] = [];
+        await bluebird.mapSeries(DOC[0].data, async (doc: any, index) => {
+            try {
+                const BARCODE: string = doc[0];
+                const QUANTITY: number = doc[1];
+
+                let _product = await Product.findOne({
+                    barcode: BARCODE,
+                    deleted: false,
+                }).exec();
+
+                if (!_product) {
+                    errors.push({
+                        barcode: BARCODE,
+                        error: 'No se encontró un producto con este código'
+                    })
+                } else {
+                    let tempSale = await TempSale.findOne({
+                        _cellar: BODY._cellar,
+                        _product: _product._id,
+                        date: BODY.date,
+                        quantity: QUANTITY
+                    }).exec();
+
+                    if (!tempSale) {
+                        errors.push({
+                            barcode: BARCODE,
+                            error: 'No se encontró un producto con este código'
+                        })
+                    } else {
+                        await TempSale.deleteOne(
+                            {
+                                _id: tempSale._id,
+                            }
+                        ).exec();
+                    }
+                }
+
+                code++;
+                console.log("🚀 ~ file: product.ts ~ line 372 ~ awaitbluebird.mapSeries ~ code", code)
+            } catch (e: any) {
+                throw new Error(e.message);
+            }
+        });
+
+        return res.status(200).json({
+            ok: true,
+            errors
+        });
+    });
+});
 /* #endregion */
 
 const SEARCH_STOCK_SALES = async (detail: any[], newStart: Date, newEnd: Date, MIN_X: any, MAX_X: any): Promise<any> => {
